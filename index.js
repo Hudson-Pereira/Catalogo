ee7809df6a68e3916e6fdf895a196aa1d090e66f
Hudson-Pereira <hudson.o.pereira@gmail.com>
2021-10-09 16:26:59 -0300

    ajustes

--- index.js
+++ index.js
@@ -10,9 +10,13 @@ app.use(express.urlencoded({ extended: true }));
 
 // variaveis
 
-let herois = [{img: "./img/sm.jpg", nome: "Superman", forca: 10, vel: 10, hab: 8, equip: 2, int: 7, poder: 10}];
+let herois = [{img: "https://cdn.ome.lt/-rlI9Dfnv9_IH6OEt_9YZTIHZ_g=/970x360/smart/uploads/conteudo/fotos/CAPA-superman.png", nome: "Superman", forca: 10, vel: 10, hab: 8, equip: 2, int: 7, poder: 10},
+              {img: "https://disneyplusbrasil.com.br/wp-content/uploads/2021/05/Homem-Aranha-1-1024x576.jpg",nome: "SpiderMan", forca: 7, vel: 8, hab: 8, equip: 6, int: 9, poder: 9},
+              {img: "https://cdn.ome.lt/ShNEEjXy2us3h_Sxe7AvZ9Vi0jA=/570x0/smart/uploads/conteudo/fotos/CAPA-hulk_Rvhcy2G.png",nome: "Hulk", forca: 10, vel: 9, hab: 4, equip: 1, int: 3, poder: 9},
+              {img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTEuwQGnO6uYyDw27xlvj4aNOOZF1QZodu5sg&usqp=CAU",nome: "IronMan", forca: 8, vel: 7, hab: 9, equip: 10, int: 10, poder: 9}
+];
 
-app.get("/", (req, res) => {
+              app.get("/", (req, res) => {
     res.redirect("/index");
 });
 
@@ -50,10 +54,8 @@ app.get("/cadastro", (req, res) => {
 });
 
 app.get("/detalhes/:id", (req, res) => {
-    const id = req.params.id
-    const novoHeroi = herois[id]
     titulo = "DETALHES";
-    res.render("detalhes", { titulo: titulo, novoHeroi });
+    res.render("detalhes", { titulo: titulo, heroi: herois[req.params.id] });
 });
 
 //liga o servidor na porta 3000
