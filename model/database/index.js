8e7f454f9ded3201809f7798ecf831ba7460ab66
Gabriel <gabrielsfarias@outlook.com>
2021-10-14 00:09:58 -0300

    add opçao de SSL para conexao com database - resolvido

--- model/database/index.js
+++ model/database/index.js
@@ -1,12 +1,17 @@
 const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize(process.env.DB_BASE, process.env.DB_USER, process.env.DB_PASS, {
-    host: process.env.DB_HOST, 
-    port: process.env.DB_PORT, 
-    dialect: 'postgres'
-});
-
-async function conectado(){
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    },
+})
+async function conectado() {
     try {
         await sequelize.authenticate();
         console.log('Connection has been established succesfully.');
@@ -15,4 +20,4 @@ async function conectado(){
     }
 }
 
-module.exports = {sequelize, conectado};
+module.exports = { sequelize, conectado };
