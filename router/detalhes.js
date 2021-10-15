31ede98323f8a6b803793f3e06c8888216760e41
Gabriel <gabrielsfarias@outlook.com>
2021-10-15 03:03:12 -0300

    imagens nao funcionam
    Inserir imagem vai exigir mais tempo ou ajuda do professor, sugiro tirar o campo imagem e fazer o link com /detalhes pelo nome, ou deixar os detalhes de lado e fazer tudo numa pagina so, com uma descriçao talvez, mas nao vejo necessidade.
    
    Deletei  as linhas de detalhes que nao funcionavam com o banco;
    
    Faltando ainda o deletar e atualizar do CRUD

--- router/detalhes.js
+++ router/detalhes.js
@@ -2,11 +2,15 @@ const express = require('express');
 
 const router = express.Router();
 
+const { heroi } = require('../model')
+
 router.get("/:id", (req, res) => {
 
-    const title = herois[req.params.id].nome;
+    const id = req.params.id
+    const heroid = heroi[id]
+    const title = heroid[req.params.id].Nome;
 
-    res.render("detalhes", { titulo: title, heroi: herois[req.params.id] });
+    res.render("detalhes", { titulo: title, heroi: heroi[req.params.id] });
 
 });
 
