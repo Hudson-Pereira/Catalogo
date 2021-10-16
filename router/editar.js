88c146ea695acd9056647035d57be35db2a1006d
Gabriel <gabrielsfarias@outlook.com>
2021-10-16 01:52:16 -0300

    inicia editar, cria rota editar

---
+++ router/editar.js
@@ -0,0 +1,30 @@
+const express = require('express')
+
+const router = express.Router()
+
+const Heroi = require('../model/herois.js')
+
+router.get("/editar/:id", async (req, res) => {
+
+    const heroi = await Heroi.findByPk(req.params.id)
+
+    res.render("../views/editar", { heroi: heroi })
+
+})
+
+router.post('/editar/:id', async (req, res) => {
+    const heroi = await Heroi.findByPk(req.params.id);
+    const { image, nome, forca, vel, hab, equip, int, poder } = req.body;
+
+    heroi.Imagem = image;
+    heroi.Nome = nome;
+    heroi.Força = forca;
+    heroi.Velocidade = vel;
+    heroi.Habilidade = hab;
+    heroi.Equipamento = equip;
+    heroi.Inteligência = int;
+    heroi.Poder = poder;
+
+    await heroi.save();
+    res.redirect("/catalogo");
+});
--- router/index.js
+++ router/index.js
@@ -4,10 +4,12 @@ const router = express.Router();
 const rotaCadastro = require("./cadastro")
 const rotaCatalogo = require("./catalogo")
 const rotaDetalhes = require("./detalhes")
+const rotaEditar = require("./editar")
 
-router.use("/cadastro", rotaCadastro);
-router.use("/catalogo", rotaCatalogo);
-router.use("/detalhes", rotaDetalhes);
+router.use("/cadastro", rotaCadastro)
+router.use("/catalogo", rotaCatalogo)
+router.use("/detalhes", rotaDetalhes)
+router.use("/editar", rotaEditar)
 
 router.get("/", (req, res) =>
     res.redirect("/index")
